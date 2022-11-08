d0a234a7204238334dc3ee34d55c3a60e31c8686
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2022-11-08 15:40:21 +0000

    feat: add full sending flow

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,34 +1,24 @@
-import Handlebars from "handlebars";
-import { convert } from "html-to-text";
-import mjml2html from "mjml";
-import nodemailer from "nodemailer";
+import { NextApiRequest } from "next";
+import { sendMail } from "../../lib/smtp";
 
-import { MJML_DEFAULT_TEMPLATE } from "../../consts";
+const handler = async (request: NextApiRequest) => {
+  const { htmlPart, textPart, from, to, subject } = request.body;
 
-const handler = async (request: any) => {
-  const transporter = nodemailer.createTransport({
-    port: 1025,
-  });
+  const messageId = sendMail({ from, to, subject, text: textPart, html: htmlPart });
 
-  const { html } = mjml2html(MJML_DEFAULT_TEMPLATE);
+  // const transporter = nodemailer.createTransport({
+  //   port: 1025,
+  // });
 
-  const template = Handlebars.compile(html);
+  // const info = await transporter.sendMail({
+  //   from,
+  //   to,
+  //   subject,
+  //   text: textPart,
+  //   html: htmlPart,
+  // });
 
-  const compiledTemplate = template({
-    order: {
-      order_details_url: "https://saleor.io",
-    },
-  });
-
-  const info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>',
-    to: "bar@example.com",
-    subject: "Hello ✔",
-    text: convert(compiledTemplate),
-    html: compiledTemplate,
-  });
-
-  console.log("Message sent: %s", info.messageId);
+  console.log("Message sent: %s", messageId);
 
   return {
     status: 200,
