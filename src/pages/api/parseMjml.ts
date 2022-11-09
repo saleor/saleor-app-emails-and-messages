9634b09a332296749ff62635422859f2a3e745f7
Wojciech <wojciech.mista@hotmail.com>
2022-11-09 12:33:52 +0000

    Add mjml lib

--- src/pages/api/parseMjml.ts
+++ src/pages/api/parseMjml.ts
@@ -1,11 +1,11 @@
-import mjml2html from "mjml";
+import compileMjml from "../../lib/mjml";
 
 const handler = async (request: any, response: any) => {
   const { mjml } = request.body;
 
-  const { html } = mjml2html(mjml);
+  const rawHtml = compileMjml(mjml);
 
-  return response.status(200).json({ rawHtml: html });
+  return response.status(200).json({ rawHtml });
 };
 
 export default handler;
